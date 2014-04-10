113250566629fe2fdaabdc092083d1ad3846f0d1
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-10 17:16:23 -0400

    Finally got the calculation to work and now the last commit for this is when i add more comments to it.

--- Week 2/Expressions_Assignment/Expressions_Industry/js/script.js
+++ Week 2/Expressions_Assignment/Expressions_Industry/js/script.js
@@ -3,7 +3,7 @@
 var width = prompt("add a value");
 var ratio = prompt("a"); 
 var rim = prompt("rim size");
-var area = ratio/100 * width * 2/25.4 + rim;  //calculating the input info 
+var area = ((Number(ratio)/100) *Number( width) * 2)/25.4 + Number(rim);  //calculating the input info 
 console.log(area); //printing it out to console.
 var result = "The size of your tires is" + area + " inches "; //result of the problem will show up here.
 alert(result);
