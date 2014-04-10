a28a999a63f81afa5dbeb0722d6edd12fdf44d20
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-10 14:37:23 -0400

    working on finishing up the final steps to this problem.

--- Week 2/Expressions_Assignment/Expressions_Industry/js/script.js
+++ Week 2/Expressions_Assignment/Expressions_Industry/js/script.js
@@ -1,8 +1,9 @@
 //Kevin 04/10/2014 Expressions
 
 var width = prompt("add a value");
-var height = prompt("");
+var heigh = prompt(""); 
+var rim = prompt("")
 var area = width * height; //calculating the inout info 
 console.log(area); //printing it out to console.
-var result = "The size of your tires are " + area + " inches ";
-alert();
+var result = "The size of your tires are " + area + " inches "; //result of the problem will show up here.
+alert(result);
