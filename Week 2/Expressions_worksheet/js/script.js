4ee40062774b9c5bdb94127b1e20e277c725370f
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-10 03:31:31 -0400

    no message

--- Week 2/Expressions_worksheet/js/script.js
+++ Week 2/Expressions_worksheet/js/script.js
@@ -5,8 +5,13 @@ var peopleatparty = 22;
 var pizzaboxesordered = 11; 
 var slicesperperson = 4;
 
-peopleatparty = peopleatparty * 4; 
+peopleatparty = peopleatparty * slicesperperson; //mul 22 to 4
+var boxofpizza = slicesperpizzabox * pizzaboxesordered; //mul 8 to 11
 
 
 //print out the output slices
+console.log(boxofpizza);
 console.log(peopleatparty);
+
+
+
