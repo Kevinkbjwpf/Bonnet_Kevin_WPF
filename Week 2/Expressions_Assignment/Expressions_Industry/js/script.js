4fe49bb44f6686757a71650b3e24d23d699a876e
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-10 16:33:02 -0400

    no message

--- Week 2/Expressions_Assignment/Expressions_Industry/js/script.js
+++ Week 2/Expressions_Assignment/Expressions_Industry/js/script.js
@@ -1,10 +1,20 @@
 //Kevin 04/10/2014 Expressions
 
-var width = prompt("add a value");
-var ratio = prompt(""); 
-var rim = prompt("");
-if (width != "" && ratio != "" && rim != "")
-var area = ratio/100 * width * 2/25.4 + rim + rim; //calculating the input info 
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<script type="text/javascript">
+
+var ratio = prompt("add a value");
+var width = prompt("a"); 
+var rim = prompt("rim size");
+var area = ratio/100 * width * 2/25.4 + rim;  //calculating the input info 
 console.log(area); //printing it out to console.
 var result = "The size of your tires is" + area + " inches "; //result of the problem will show up here.
 alert(result);
+
+</body>
+
+</html>
