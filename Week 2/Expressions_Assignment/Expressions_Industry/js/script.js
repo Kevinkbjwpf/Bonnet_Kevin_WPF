b19da96a2af4668e181078773fc406b37fbdd0be
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-10 15:00:04 -0400

    no message

--- Week 2/Expressions_Assignment/Expressions_Industry/js/script.js
+++ Week 2/Expressions_Assignment/Expressions_Industry/js/script.js
@@ -1,9 +1,9 @@
 //Kevin 04/10/2014 Expressions
 
 var width = prompt("add a value");
-var heigh = prompt(""); 
+var ratio = prompt(""); 
 var rim = prompt("")
-var area = width * height; //calculating the inout info 
+var area = width * ratio/100 * 2/ 25.4 + rim; //calculating the inout info 
 console.log(area); //printing it out to console.
 var result = "The size of your tires are " + area + " inches "; //result of the problem will show up here.
 alert(result);
