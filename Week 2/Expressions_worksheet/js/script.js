9f3447fc1df582a637ef3826b4e7df2624ed627f
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-10 23:42:21 -0400

    Worksheet for Expression Slice of Pie part 1.

--- Week 2/Expressions_worksheet/js/script.js
+++ Week 2/Expressions_worksheet/js/script.js
@@ -8,8 +8,6 @@ var slicesperperson = 4;
 peopleatparty = peopleatparty * slicesperperson; //mul 22 to 4
 var boxofpizza = slicesperpizzabox * pizzaboxesordered; //mul 8 to 11
 
-
-//print out the output slices
 console.log(boxofpizza);
 console.log(peopleatparty);
 
