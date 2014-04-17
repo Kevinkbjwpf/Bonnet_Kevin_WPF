99601e93e44d718ed502d2bbb99d21098f4bfb5d
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-17 14:14:22 -0400

    Conditional Assignment work starting and putting things together.

---
+++ Week 3/Conditional Assignment/Conditionals_Wacky/js/script.js
@@ -0,0 +1,12 @@
+//Kevin 04/17/2014 Conditionals Wacky
+
+var  = prompt(":"); //this number is asking.
+var  = prompt(":"); 
+
+var  = 
+console.log(); //printing it out to console.
+var result = " "; //result of the problem will show up here.
+alert(result);
+
+
+//now working on the conditional statement of proof for the prompts
