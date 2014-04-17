16114ccedce548ad9abf0435fa6b5574942b3e69
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-17 15:36:16 -0400

    no message

--- Week 3/Conditional Assignment/Conditionals_Industy/js/script.js
+++ Week 3/Conditional Assignment/Conditionals_Industy/js/script.js
@@ -4,17 +4,19 @@
 
 //now working on the conditional statement of proof for the prompts
 
-var oldEnough = 17;
+var childAge = 15;
+var minAge = 17;
+var wParentGuardianAge = 21; // the age of the parent/guardian of the present child
 
 //if a person is old enough to be permitted in,"printing to console "you can watch this movie!"
 //if the person is 17 and older 
-if(oldEnough > 17){
+if(childAge > minAge){
 	//code will take place if the conditions are true
 	//you can watch the movie!
 	console.log("You can watch this movie!");
-}else if(){
+}else if(childAge > wParentGuardianAge ){
 //you can watch the movie with a parent/guradian if present!
-	console.log("You can watch this movie, but only with a parent/guradian present!");
+	console.log("You can watch this movie, but only with a parent/guradian present.");
 }else{
 	//sorry your just not old enough to watch this movie!
 	console.log("Sorry your just not old enough to watch this movie!");
