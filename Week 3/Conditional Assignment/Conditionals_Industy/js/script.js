81dd913903c704c8e60bbfcd7ceb0b8661566b7b
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-17 14:29:51 -0400

    no message

--- Week 3/Conditional Assignment/Conditionals_Industy/js/script.js
+++ Week 3/Conditional Assignment/Conditionals_Industy/js/script.js
@@ -1,12 +1,13 @@
 //Kevin 04/17/2014 Conditionals Industry
 
-var  = prompt(":"); //this number is asking.
-var  = prompt(":"); 
-
-var  = 
-console.log(); //printing it out to console.
-var result = " "; //result of the problem will show up here.
-alert(result);
-
+//Old enough to watch a "Rated-R" movie
 
 //now working on the conditional statement of proof for the prompts
+
+var oldEnough = true;
+
+//if a person is old enough to be permitted in,"printing to console "you can watch this movie!"
+if(oldEnough){
+	//code will take place if the conditions are true
+	console.log("You can watch this movie!");
+}
