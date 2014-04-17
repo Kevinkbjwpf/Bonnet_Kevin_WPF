b3d0de8b7a6dfc9adbfc823ae730d0eef182b6fc
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-17 12:24:34 -0400

    no message

--- Week 3/Conditionals_Worksheet/js/script.js
+++ Week 3/Conditionals_Worksheet/js/script.js
@@ -8,13 +8,28 @@ var movieTicketPrices = ["age" , "time"];
 var totalNumPrices = 0;
 
 console.log(movieTicketPrices);
+console.log(movieTicketPrices[1]);
 
 //making a loop "ForLoop" : Basic Strcuture - for(declare variable for counting; Condition to test; what happens after the loop) [Code Inside Here]
 
 //testing the items in the array by using the ForLoop
 
-for(var i=0; i<3; i++ )
+//now testing using the property of an array: /lenght  -give us the number of items inside of the array
+
+for(var i=0; i<movieTicketPrices.length; i++ )
 { console.log("inside the for loop"); 
-}
+
+console.log(movieTicketPrices[i]);
+
+//testing the different items in the array and getting the movie prices
+//iF statements 
+//iF(Thing to test) {CODE TO RUN IF TRUE}
+   
+    if(movieTicketPrices[1] ==="prices");{
+	    console.log("item is a price");
+	    //keeping count of the number of prices
+	    totalNumPrices++;
+    }
+   }
 
      console.log("After the For Loop");
