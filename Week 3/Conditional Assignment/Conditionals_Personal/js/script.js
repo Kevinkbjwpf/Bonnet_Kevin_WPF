976a94d05fb5b5ac5114dfb74a8d8cb19c3e9e8e
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-17 15:12:16 -0400

    Starting personal conditional work were I'm going to checking on Age limit to have a drink of alcohol

--- Week 3/Conditional Assignment/Conditionals_Personal/js/script.js
+++ Week 3/Conditional Assignment/Conditionals_Personal/js/script.js
@@ -1,12 +1,17 @@
-//Kevin 04/17/2014 Conditionals
-
-var  = prompt(":"); //this number is asking.
-var  = prompt(":"); 
-
-var  = 
-console.log(); //printing it out to console.
-var result = " "; //result of the problem will show up here.
-alert(result);
+//Kevin 04/17/2014 Conditionals Personal
 
 
 //now working on the conditional statement of proof for the prompts
+
+var personAge = 19;
+var minAge = 21
+
+//if a person is old enough to be permitted in,"printing to console "you can drink alcohol!"
+//if the person is 21 and older 
+if(personAge > minAge){
+	//code will take place if the conditions are true
+	console.log("You can have a drink!");
+}else{
+//code will be perform if conditionals is false
+	console.log("Sorry your just not old enough to have alcohol yet!");
+}
