5c05f95b36c1ad96380acfb805b9370514a35216
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-17 11:48:14 -0400

    my conditional would be on finding out movie ticket prices.

---
+++ Week 3/Conditionals_Worksheet/js/script.js
@@ -0,0 +1,10 @@
+//Checking movie ticket prices
+
+//create a variable that represent movie ticket prices
+
+var movieTicketPrices = ["age" , "time"];
+
+//varibale that will hold the number for ticket prices
+var totalNumPrices = 0;
+
+console.log(movieTicketPrices);
