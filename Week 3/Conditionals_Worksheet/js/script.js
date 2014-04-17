c913f6879d961c98499da4b57f1b4983d098cbae
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-17 12:03:05 -0400

    using ForLoop to test are items in are array

--- Week 3/Conditionals_Worksheet/js/script.js
+++ Week 3/Conditionals_Worksheet/js/script.js
@@ -8,3 +8,13 @@ var movieTicketPrices = ["age" , "time"];
 var totalNumPrices = 0;
 
 console.log(movieTicketPrices);
+
+//making a loop "ForLoop" : Basic Strcuture - for(declare variable for counting; Condition to test; what happens after the loop) [Code Inside Here]
+
+//testing the items in the array by using the ForLoop
+
+for(var i=0; i<3; i++ )
+{ console.log("inside the for loop"); 
+}
+
+     console.log("After the For Loop");
