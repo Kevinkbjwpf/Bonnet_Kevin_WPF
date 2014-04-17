89e813abc153179690c02229659120ead3a7da88
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-17 19:40:02 -0400

    setting up my prompts to ask and collect user data

--- Week 3/Conditional Assignment/Conditionals_Industy/js/script.js
+++ Week 3/Conditional Assignment/Conditionals_Industy/js/script.js
@@ -1,20 +1,20 @@
-//Kevin 04/17/2014 Conditionals Industry
+ //Kevin 04/17/2014 Conditionals Industry
 
 //Old enough to watch a "Rated-R" movie
 
 //now working on the conditional statement of proof for the prompts
 
-var childAge = 15;
+var Age = 15;
 var minAge = 17;
 var wParentGuardianAge = 21; // the age of the parent/guardian of the present child
 
 //if a person is old enough to be permitted in,"printing to console "you can watch this movie!"
 //if the person is 17 and older 
-if(childAge > minAge){
+if(Age > minAge){
 	//code will take place if the conditions are true
 	//you can watch the movie!
 	console.log("You can watch this movie!");
-}else if(childAge > wParentGuardianAge ){
+}else if(Age > wParentGuardianAge ){
 //you can watch the movie with a parent/guradian if present!
 	console.log("You can watch this movie, but only with a parent/guradian present.");
 }else{
