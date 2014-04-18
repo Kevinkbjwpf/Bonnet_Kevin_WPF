9894c88c77654a8e4cb1866a768aadec4d3e0469
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-04-17 20:42:04 -0400

    I don't know what happen with the last commit for this. It was for the wrong folder files

--- Week 3/Conditional Assignment/Conditionals_Wacky/js/script.js
+++ Week 3/Conditional Assignment/Conditionals_Wacky/js/script.js
@@ -1,42 +1,9 @@
 //Kevin 04/17/2014 Conditionals Wacky
+var  = prompt(":"); //this number is asking.
+var  = prompt(":"); 
 
-//Checking movie ticket prices
+var  = 
+console.log(); //printing it out to console.
+var result = " "; //result of the problem will show up here.
+alert(result);
 
-//create a variable that represent movie ticket prices
-
-var movieTicketPrices = ["age" , "time"];
-
-//varibale that will hold the number for ticket prices
-var totalNumPrices = 0;
-
-console.log(movieTicketPrices);
-console.log(movieTicketPrices[1]);
-
-//making a loop "ForLoop" : Basic Strcuture - for(declare variable for counting; Condition to test; what happens after the loop) [Code Inside Here]
-
-//testing the items in the array by using the ForLoop
-
-//now testing using the property of an array: /lenght  -give us the number of items inside of the array
-
-for(var i=0; i<movieTicketPrices.length; i++ )
-{ console.log("inside the for loop"); 
-
-console.log(movieTicketPrices[i]);
-
-//testing the different items in the array and getting the movie prices
-//iF statements 
-//iF(Thing to test) {CODE TO RUN IF TRUE}
-   
-    if(movieTicketPrices[1] ==="prices");{
-    //iF item is a movie ticket price it will run this 
-	    console.log("item is a price");
-	    //keeping count of the number of prices
-	    totalNumPrices++;
-    }
-   }
-
-     console.log("After the For Loop");
-     console.log(totalNumPrices);
-     
-console.log
-alert("There are "+totalNumPrices+" different prices(s) for movie tickets");     
