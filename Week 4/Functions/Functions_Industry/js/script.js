966f9238cff95e6388cc3bcf980b01cbf32545eb
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-05-01 03:37:59 -0400

    no message

--- Week 4/Functions/Functions_Industry/js/script.js
+++ Week 4/Functions/Functions_Industry/js/script.js
@@ -9,3 +9,26 @@ var totalNumPrices = 0;
 
 console.log(movieTicketPrices);
 console.log(movieTicketPrices[1]);
+
+totalNumPrices=chechForPrices(movieTicketPrices);
+
+console.log("There are "+totalNumPrices+" different prices(s) for movie tickets");
+
+//creating a functions to cycle through the array and counts the movie ticket prices.
+
+function checkForPrices(movieTickets){
+	
+	console.log(movieTickets[2]);
+	
+	//create a varibale to count the tickets
+	var numTickets = 0;
+	
+	//check each item in the array to see if it is a ticket
+	
+	for(var i=0; i<movieTicketPrices.length; i++ )
+{ 
+//console.log(movieTicketPrices[i]); 
+//test the movie tickets prices
+if(movieTicketPrices[i] === ""
+	
+}
