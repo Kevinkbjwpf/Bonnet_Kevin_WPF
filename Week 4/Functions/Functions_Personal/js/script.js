416e67eaac67b461eb9eff752442f4f82db8e367
Kevin Bonnet <kevinbonnet58@gmail.com>
2014-05-01 19:10:02 -0400

    Personal Functions. kind of having a much harder way with functions a little bit even if it seems to be the easiest.

--- Week 4/Functions/Functions_Personal/js/script.js
+++ Week 4/Functions/Functions_Personal/js/script.js
@@ -1,5 +1,21 @@
 //JavaScript Functions Personal 05/012014
 
-var
+function outptMsg(){
+	console.log("What's Going on");
+}
+//having 2 of the same variable and trying to calculate both seperately.
 
-console.log();
+var gasPrice = 3.27;
+
+function calcArea(){
+	var distance = 960;
+	var gasMileage = 25;
+	var gasPrice = 3.49;
+	var gallons = 40;
+	var totalMoney = gasPrice * gallons;
+	//console.log(totalMoney);
+}
+
+calcArea();
+console.log(gasPrice);
+
